e35d436e2584a9e34be44351e859bda30285bfcf
Leon <ajer333@gmail.com>
2023-05-01 13:28:21 +0300

    ret

--- src/ManageScreen.java
+++ src/ManageScreen.java
@@ -5,20 +5,64 @@ public class ManageScreen extends JPanel {
     private JComboBox<String> survivedComboBox;
 
     public ManageScreen(int x, int y, int width, int height) {
-        File file = new File(Constants.PATH_TO_DATA_FILE); //this is the path to the data file
+        File file = new File(Constants.PATH_TO_DATA_FILE);
         if (file.exists()) {
+
             this.setLayout(null);
             this.setBounds(x, y + Constants.MARGIN_FROM_TOP, width, height);
+
             JLabel survivedLabel = new JLabel("Passenger Class: ");
             survivedLabel.setBounds(x + Constants.MARGIN_FROM_LEFT, y, Constants.LABEL_WIDTH, Constants.LABEL_HEIGHT);
             this.add(survivedLabel);
+
             this.survivedComboBox = new JComboBox<>(Constants.PASSENGER_CLASS_OPTIONS);
             this.survivedComboBox.setBounds(survivedLabel.getX() + survivedLabel.getWidth() + 1, survivedLabel.getY(), Constants.COMBO_BOX_WIDTH, Constants.COMBO_BOX_HEIGHT);
             this.add(this.survivedComboBox);
+
             this.survivedComboBox.addActionListener((e) -> {
-                //do whatever you want on change
+                int selected = survivedComboBox.getSelectedIndex();
+                getPClassSurvive(selected);
             });
+            survivedComboBox.revalidate();
         }
     }
+    public void getPClassSurvive(int index) {
+        int survived = 0;
+        int notSurvived = 0;
+        for (Passenger pas : Constants.passengers) {
+            switch (index) {
+                case 0:
+                    if (pas.getSurvive().equals("1")) {
+                        survived++;
+                    } else {
+                        notSurvived++;
+                    }
+                    break;
+                case 1:
+                    if (pas.getSurvive().equals("1") && pas.getpClass().equals("1")) {
+                        survived++;
+                    } else if(pas.getSurvive().equals("0") && pas.getpClass().equals("1")){
+                        notSurvived++;
+                    }
+                    break;
 
+                case 2:
+                    if (pas.getSurvive().equals("1") && pas.getpClass().equals("2")) {
+                        survived++;
+                    } else if(pas.getSurvive().equals("0") && pas.getpClass().equals("2")){
+                        notSurvived++;
+                    }
+                    break;
+
+                case 3:
+                    if (pas.getSurvive().equals("1") && pas.getpClass().equals("3")) {
+                        survived++;
+                    } else if(pas.getSurvive().equals("0") && pas.getpClass().equals("3")){
+                        notSurvived++;
+                    }
+                    break;
+            }
+        }
+        System.out.println("total: " + (survived + notSurvived) + " (survived: " + survived + ", not survived: " + notSurvived + ")");
+    }
 }
--- src/Titanic.java
+++ src/Titanic.java
@@ -1,21 +1,44 @@
 import javax.swing.*;
+import java.io.*;
 
 class Titanic extends JFrame {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         new Titanic();
+        excelReader();
     }
 
+    public static void excelReader() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(Constants.PATH_TO_DATA_FILE));
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                Passenger passenger = new Passenger(values);
+                Constants.passengers.add(passenger);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода: " + e.getMessage());
+        }
+    }
     public Titanic() {
         this.setTitle("Titanic Passengers Data");
         this.setLayout(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setSize(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
         this.add(new ManageScreen(0, 0, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new TextStringID(0, 60, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new TextSringName(0, 90, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new ManageScreenGender(0, 120, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new TextStringSib(0, 180, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new TextStringParch(0, 210, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+        this.add(new TextFieldCost(0, 240, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT));
+
         this.setVisible(true);
 
     }
-
-
-
 }
